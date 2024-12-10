0988992f8819636f6f668c358dec19056101ab5f
iulic876 <botnaruiulian388@gmail.com>
2024-12-10 13:41:43 +0200

    initial

---
+++ apple.test1/src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./assets/Header";
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Hwllo</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ apple.test1/src/assets/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+  return (
+    <div>
+      <h1 className="text-6xl font-extrabold">This is the header</h1>
+    </div>
+  );
+};
+export default Header;
